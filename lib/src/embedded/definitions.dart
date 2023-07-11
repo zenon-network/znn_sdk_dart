efa5a9e5bbf10714f0222b115d555bacc5c8628f
Sol Sanctum <so1_sanctum@pm.me>
2023-07-10 20:20:40 -0400

    Corrected spacing in contract definitions

--- lib/src/embedded/definitions.dart
+++ lib/src/embedded/definitions.dart
@@ -65,27 +65,27 @@ class Definitions {
   ]''';
 
   static final String _bridgeDefinition = '''[
-		{"type":"function","name":"WrapToken","inputs":[{"name":"networkClass","type":"uint32"},{"name":"chainId","type":"uint32"},{"name":"toAddress","type":"string"}]},
-		{"type":"function","name":"UpdateWrapRequest","inputs":[{"name":"id","type":"hash"},{"name":"signature","type":"string"}]},
-		{"type":"function","name":"SetNetwork","inputs":[{"name":"networkClass","type":"uint32"},{"name":"chainId","type":"uint32"},{"name":"name","type":"string"},{"name":"contractAddress","type":"string"},{"name":"metadata","type":"string"}]},
-		{"type":"function","name":"RemoveNetwork","inputs":[{"name":"networkClass","type":"uint32"},{"name":"chainId","type":"uint32"}]},
-		{"type":"function","name":"SetTokenPair","inputs":[{"name":"networkClass","type":"uint32"},{"name":"chainId","type":"uint32"},{"name":"tokenStandard","type":"tokenStandard"},{"name":"tokenAddress","type":"string"},{"name":"bridgeable","type":"bool"},{"name":"redeemable","type":"bool"},{"name":"owned","type":"bool"},{"name":"minAmount","type":"uint256"},{"name":"feePercentage","type":"uint32"},{"name":"redeemDelay","type":"uint32"},{"name":"metadata","type":"string"}]},
-		{"type":"function","name":"SetNetworkMetadata","inputs":[{"name":"networkClass","type":"uint32"},{"name":"chainId","type":"uint32"},{"name":"metadata","type":"string"}]},
-		{"type":"function","name":"RemoveTokenPair","inputs":[{"name":"networkClass","type":"uint32"},{"name":"chainId","type":"uint32"},{"name":"tokenStandard","type":"tokenStandard"},{"name":"tokenAddress","type":"string"}]},
-		{"type":"function","name":"Halt","inputs":[{"name":"signature","type":"string"}]},
-		{"type":"function","name":"Unhalt","inputs":[]},
-		{"type":"function","name":"Emergency","inputs":[]},
-		{"type":"function","name":"ChangeTssECDSAPubKey","inputs":[{"name":"pubKey","type":"string"},{"name":"oldPubKeySignature","type":"string"},{"name":"newPubKeySignature","type":"string"}]},
-		{"type":"function","name":"ChangeAdministrator","inputs":[{"name":"administrator","type":"address"}]},
-		{"type":"function","name":"ProposeAdministrator","inputs":[{"name":"address","type":"address"}]},
-		{"type":"function","name":"SetAllowKeyGen","inputs":[{"name":"allowKeyGen","type":"bool"}]},
-		{"type":"function","name":"SetRedeemDelay","inputs":[{"name":"redeemDelay","type":"uint64"}]},
-		{"type":"function","name":"SetBridgeMetadata","inputs":[{"name":"metadata","type":"string"}]},
-		{"type":"function","name":"UnwrapToken","inputs":[{"name":"networkClass","type":"uint32"},{"name":"chainId","type":"uint32"},{"name":"transactionHash","type":"hash"},{"name":"logIndex","type":"uint32"},{"name":"toAddress","type":"address"},{"name":"tokenAddress","type":"string"},{"name":"amount","type":"uint256"},{"name":"signature","type":"string"}]},
-		{"type":"function","name":"RevokeUnwrapRequest","inputs":[{"name":"transactionHash","type":"hash"},{"name":"logIndex","type":"uint32"}]},
-		{"type":"function","name":"Redeem","inputs":[{"name":"transactionHash","type":"hash"},{"name":"logIndex","type":"uint32"}]},
-		{"type":"function","name":"NominateGuardians","inputs":[{"name":"guardians","type":"address[]"}]},
-		{"type":"function","name":"SetOrchestratorInfo","inputs":[{"name":"windowSize","type":"uint64"},{"name":"keyGenThreshold","type":"uint32"},{"name":"confirmationsToFinality","type":"uint32"},{"name":"estimatedMomentumTime","type":"uint32"}]}
+    {"type":"function","name":"WrapToken","inputs":[{"name":"networkClass","type":"uint32"},{"name":"chainId","type":"uint32"},{"name":"toAddress","type":"string"}]},
+    {"type":"function","name":"UpdateWrapRequest","inputs":[{"name":"id","type":"hash"},{"name":"signature","type":"string"}]},
+    {"type":"function","name":"SetNetwork","inputs":[{"name":"networkClass","type":"uint32"},{"name":"chainId","type":"uint32"},{"name":"name","type":"string"},{"name":"contractAddress","type":"string"},{"name":"metadata","type":"string"}]},
+    {"type":"function","name":"RemoveNetwork","inputs":[{"name":"networkClass","type":"uint32"},{"name":"chainId","type":"uint32"}]},
+    {"type":"function","name":"SetTokenPair","inputs":[{"name":"networkClass","type":"uint32"},{"name":"chainId","type":"uint32"},{"name":"tokenStandard","type":"tokenStandard"},{"name":"tokenAddress","type":"string"},{"name":"bridgeable","type":"bool"},{"name":"redeemable","type":"bool"},{"name":"owned","type":"bool"},{"name":"minAmount","type":"uint256"},{"name":"feePercentage","type":"uint32"},{"name":"redeemDelay","type":"uint32"},{"name":"metadata","type":"string"}]},
+    {"type":"function","name":"SetNetworkMetadata","inputs":[{"name":"networkClass","type":"uint32"},{"name":"chainId","type":"uint32"},{"name":"metadata","type":"string"}]},
+    {"type":"function","name":"RemoveTokenPair","inputs":[{"name":"networkClass","type":"uint32"},{"name":"chainId","type":"uint32"},{"name":"tokenStandard","type":"tokenStandard"},{"name":"tokenAddress","type":"string"}]},
+    {"type":"function","name":"Halt","inputs":[{"name":"signature","type":"string"}]},
+    {"type":"function","name":"Unhalt","inputs":[]},
+    {"type":"function","name":"Emergency","inputs":[]},
+    {"type":"function","name":"ChangeTssECDSAPubKey","inputs":[{"name":"pubKey","type":"string"},{"name":"oldPubKeySignature","type":"string"},{"name":"newPubKeySignature","type":"string"}]},
+    {"type":"function","name":"ChangeAdministrator","inputs":[{"name":"administrator","type":"address"}]},
+    {"type":"function","name":"ProposeAdministrator","inputs":[{"name":"address","type":"address"}]},
+    {"type":"function","name":"SetAllowKeyGen","inputs":[{"name":"allowKeyGen","type":"bool"}]},
+    {"type":"function","name":"SetRedeemDelay","inputs":[{"name":"redeemDelay","type":"uint64"}]},
+    {"type":"function","name":"SetBridgeMetadata","inputs":[{"name":"metadata","type":"string"}]},
+    {"type":"function","name":"UnwrapToken","inputs":[{"name":"networkClass","type":"uint32"},{"name":"chainId","type":"uint32"},{"name":"transactionHash","type":"hash"},{"name":"logIndex","type":"uint32"},{"name":"toAddress","type":"address"},{"name":"tokenAddress","type":"string"},{"name":"amount","type":"uint256"},{"name":"signature","type":"string"}]},
+    {"type":"function","name":"RevokeUnwrapRequest","inputs":[{"name":"transactionHash","type":"hash"},{"name":"logIndex","type":"uint32"}]},
+    {"type":"function","name":"Redeem","inputs":[{"name":"transactionHash","type":"hash"},{"name":"logIndex","type":"uint32"}]},
+    {"type":"function","name":"NominateGuardians","inputs":[{"name":"guardians","type":"address[]"}]},
+    {"type":"function","name":"SetOrchestratorInfo","inputs":[{"name":"windowSize","type":"uint64"},{"name":"keyGenThreshold","type":"uint32"},{"name":"confirmationsToFinality","type":"uint32"},{"name":"estimatedMomentumTime","type":"uint32"}]}
   ]''';
 
   static final String _liquidityDefinition = '''[
@@ -114,10 +114,10 @@ class Definitions {
     {"type":"function","name":"Update","inputs":[]},
     {"type":"function","name":"Donate","inputs":[]},
     {"type":"function","name":"VoteByName","inputs":[{"name":"id","type":"hash"},{"name":"name","type":"string"},{"name":"vote","type":"uint8"}]},
-		{"type":"function","name":"VoteByProdAddress","inputs":[{"name":"id","type":"hash"},{"name":"vote","type":"uint8"}]},
-		{"type":"variable","name":"timeChallengeInfo","inputs":[{"name":"methodName","type":"string"},{"name":"paramsHash","type":"hash"},{"name":"challengeStartHeight","type":"uint64"}]},
-		{"type":"variable","name":"securityInfo","inputs":[{"name":"guardians","type":"address[]"},{"name":"guardiansVotes","type":"address[]"},{"name":"administratorDelay","type":"uint64"},{"name":"softDelay","type":"uint64"}]}
-	]''';
+    {"type":"function","name":"VoteByProdAddress","inputs":[{"name":"id","type":"hash"},{"name":"vote","type":"uint8"}]},
+    {"type":"variable","name":"timeChallengeInfo","inputs":[{"name":"methodName","type":"string"},{"name":"paramsHash","type":"hash"},{"name":"challengeStartHeight","type":"uint64"}]},
+    {"type":"variable","name":"securityInfo","inputs":[{"name":"guardians","type":"address[]"},{"name":"guardiansVotes","type":"address[]"},{"name":"administratorDelay","type":"uint64"},{"name":"softDelay","type":"uint64"}]}
+  ]''';
 
   // ABI definitions of embedded contracts
   static final Abi plasma = Abi.fromJson(_plasmaDefinition);
